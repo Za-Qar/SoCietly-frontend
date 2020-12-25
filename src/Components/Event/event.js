1337febcbb29665dacf64f189468069d55ab74e4
= <za.qa@outlook.com>
2020-12-25 16:48:49 +0000

    completion render complete

--- src/Components/Event/event.js
+++ src/Components/Event/event.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 import { useUserContext } from "../../Context/userContext";
@@ -6,6 +6,7 @@ import { useUserContext } from "../../Context/userContext";
 function Event() {
   const [user] = useUserContext();
   const { register, handleSubmit, watch, errors } = useForm();
+  const [complete, setComplete] = useState(false);
 
   let createEvent = (msg) => {
     console.log("User Input recieved", msg);
@@ -30,64 +31,74 @@ function Event() {
       .then((res) => res.json())
       .then((data) => console.log("this is the user data: ", data))
       .catch((error) => console.log("user creation error error: ", error));
+
+    setComplete(true);
   };
 
   function consoleLog() {
     console.log(user);
   }
 
-  return (
-    <div>
-      <button onClick={consoleLog}>Get User</button>
-      <form onSubmit={handleSubmit(createEvent)}>
-        <span>
-          <p>Event Name:</p>
-          <input name="eventName" ref={register} required />
-        </span>
-        <span>
-          <p>Event Type:</p>
-          <select id="eventTypes" name="eventTypes" ref={register}>
-            <option value="education">Education</option>
-            <option value="social">Social</option>
-            <option value="community">Community</option>
-          </select>
-        </span>
-        <span>
-          <p>Date:</p>
-          <input name="date" type="date" ref={register} required />
-        </span>
-        <span>
-          <p>Time:</p>
-          <input name="time" type="time" ref={register} required />
-        </span>
-        <span>
-          <p>Description:</p>
-          <textarea
-            name="description"
-            rows="10"
-            cols="30"
-            ref={register}
-          ></textarea>
-        </span>
-        <span>
-          <p>Image:</p>
-          <input name="image" ref={register} required />
-        </span>
-        <span>
-          <p>Location:</p>
-          <input name="location" ref={register} required />
-        </span>
-        <span>
-          <p>Volunteers:</p>
-          <select id="eventVolunteers" name="eventVolunteers" ref={register}>
-            <option value="true">Yes</option>
-            <option value="false">No</option>
-          </select>
-        </span>
-        <input type="submit" />
-      </form>
-    </div>
-  );
+  if (complete === false) {
+    return (
+      <div>
+        <button onClick={consoleLog}>Get User</button>
+        <form onSubmit={handleSubmit(createEvent)}>
+          <span>
+            <p>Event Name:</p>
+            <input name="eventName" ref={register} required />
+          </span>
+          <span>
+            <p>Event Type:</p>
+            <select id="eventTypes" name="eventTypes" ref={register}>
+              <option value="education">Education</option>
+              <option value="social">Social</option>
+              <option value="community">Community</option>
+            </select>
+          </span>
+          <span>
+            <p>Date:</p>
+            <input name="date" type="date" ref={register} required />
+          </span>
+          <span>
+            <p>Time:</p>
+            <input name="time" type="time" ref={register} required />
+          </span>
+          <span>
+            <p>Description:</p>
+            <textarea
+              name="description"
+              rows="10"
+              cols="30"
+              ref={register}
+            ></textarea>
+          </span>
+          <span>
+            <p>Image:</p>
+            <input name="image" ref={register} required />
+          </span>
+          <span>
+            <p>Location:</p>
+            <input name="location" ref={register} required />
+          </span>
+          <span>
+            <p>Volunteers:</p>
+            <select id="eventVolunteers" name="eventVolunteers" ref={register}>
+              <option value="true">Yes</option>
+              <option value="false">No</option>
+            </select>
+          </span>
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  } else if (complete === true) {
+    return (
+      <div>
+        <p>Completed confirmation design comes here</p>
+      </div>
+    );
+  }
 }
 
 export default Event;
