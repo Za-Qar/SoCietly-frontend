8b9d42ee8e1c8eb9c17782c26e4417af4be33ba4
Luke Fantom <luke.fantom@gmail.com>
2020-12-29 15:18:49 +0000

    cleaned up authguard

--- src/Hoc/authGuard.js
+++ src/Hoc/authGuard.js
@@ -16,13 +16,13 @@ export default function UserSignIn({
   exact = false,
 }) {
   const [authUser, loading, error] = useAuthContext();
-  const [user, setUser] = useUserContext();
+  // user info returned from firebase - context
 
   const [userData, setUserData] = useState();
+  // user data returned from backend
 
-  console.log({ userData });
-  console.log({ authUser });
-  console.log({ user });
+  const [user, setUser] = useUserContext();
+  // user context set with firebase and backend data - context
 
   useEffect(() => {
     async function getUser() {
@@ -36,37 +36,6 @@ export default function UserSignIn({
     }
     getUser();
   }, [authUser]);
-
-  // async function getUser() {
-  //   if (authUser) {
-  //     let res = await fetch(
-  //       `http://localhost:3000/user/email/${authUser.email}`
-  //     );
-  //     let data = await res.json();
-  //     setUserData(data.payload[0]);
-
-  //     const newUser = {
-  //       uid: userData.id,
-  //       username: userData.name,
-  //       email: authUser.email,
-  //       profileImage: authUser.photoURL,
-  //       lastSignIn: authUser.metadata.lastSignInTime,
-  //       admin: userData.admin,
-  //       cohort: userData.cohort,
-  //       currentRole: userData.currentrole,
-  //       currentEmployer: userData.currentemployer,
-  //       skills: userData.skills,
-  //       introduction: userData.introduction,
-  //       journey: props.journey,
-  //     };
-  //     setUser(newUser);
-  //   }
-  // }
-
-  function logging() {
-    console.log("it is logging");
-    console.log(userData);
-  }
 
   useEffect(() => {
     if (authUser && userData) {
@@ -97,57 +66,4 @@ export default function UserSignIn({
   ) : (
     <Redirect to={"/login"}></Redirect>
   );
-
-  // return <button onClick={logging}>Click to log user</button>;
 }
-
-// //React
-// import React, { useEffect, useState } from "react";
-// import { Redirect, Route } from "react-router-dom";
-
-// //Context
-// import { useAuthContext } from "../Context/authContext";
-// import { useUserContext } from "../Context/userContext";
-
-// //Component
-// import Loading from "../Components/Loading/loading";
-
-// export default function UserSignIn({
-//   component: Component,
-//   path = "/",
-//   props,
-//   exact = false,
-// }) {
-//   const [authUser, loading, error] = useAuthContext();
-//   const [user, setUser] = useUserContext();
-
-//   useEffect(() => {
-//     if (authUser) {
-//       const newUser = {
-//         uid: props.uid,
-//         username: authUser.displayName,
-//         email: authUser.email,
-//         profileImage: authUser.photoURL,
-//         lastSignIn: authUser.metadata.lastSignInTime,
-//         admin: props.admin,
-//         cohort: props.cohort,
-//         currentRole: props.currentRole,
-//         currentEmployer: props.currentEmployer,
-//         skills: props.skills,
-//         introduction: props.introduction,
-//         journey: props.journey,
-//       };
-//       setUser(newUser);
-//     }
-//   }, [authUser]);
-
-//   if (loading) {
-//     return <Loading />;
-//   }
-
-//   return authUser ? (
-//     <Route path={path} render={() => <Component />} exact={exact}></Route>
-//   ) : (
-//     <Redirect to={"/login"}></Redirect>
-//   );
-// }
