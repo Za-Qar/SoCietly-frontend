8858df5895872da0d2bfc6d94125c3d99077f95d
Luke Fantom <luke.fantom@gmail.com>
2020-12-30 11:48:13 +0000

    closes FAL90-91

--- src/Pages/Homepage/homepage.js
+++ src/Pages/Homepage/homepage.js
@@ -1,5 +1,8 @@
 //components
-import { logout } from "../../Components/Firebase/auth";
+// import { logout } from "../../Components/Firebase/auth";
+import UserInfo from "../../Components/UserInfo/userinfo";
+import UserImage from "../../Components/userImage/userImage";
+import JobUpdates from "../../Components/jobUpdates/jobUpdates";
 
 //Context
 import { useUserContext } from "../../Context/userContext";
@@ -7,17 +10,19 @@ import { useUserContext } from "../../Context/userContext";
 export default function Homepage() {
   const [user, setUser] = useUserContext();
 
-  function signOut() {
-    logout();
-    setUser(null);
-  }
+  // function signOut() {
+  //   logout();
+  //   setUser(null);
+  // }
 
   return (
     user && (
       <div>
         <h1>Homepage</h1>
-        <p>Hello {user.username}</p>
-        <button onClick={signOut}>Log Out</button>
+        {/* <button onClick={signOut}>Log Out</button> */}
+        <UserImage user={user} />
+        <UserInfo user={user} />
+        <JobUpdates />
       </div>
     )
   );
