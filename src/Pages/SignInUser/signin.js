16141e128f48dffd251262038585aacf10358ba0
Luke Fantom <luke.fantom@gmail.com>
2021-01-05 10:25:23 +0000

    updated sign up

--- src/Pages/SignInUser/signin.js
+++ src/Pages/SignInUser/signin.js
@@ -30,19 +30,8 @@ export default function UserSignIn() {
   ) : (
     <div>
       <h1>Sign up or Login</h1>
-      <h2>Hello random person</h2>
-      <h3>Hello random person</h3>
-      <h4>Hello random person</h4>
-      <h5>Hello random person</h5>
-      <h6>Hello random person</h6>
-      <p>Hello random person</p>
+
       <button onClick={handleGoogle}>Continue with Google</button>
-      {/* <form action="https://google.com">
-        <input type="submit" value="Sign Up" />
-      </form> */}
-      {/* <Link to="/signup">
-        <button>Continue with Google</button>
-      </Link> */}
     </div>
   );
 }
