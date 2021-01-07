b4af0db965797ccda2568752f69e7309e5666a74
= <za.qa@outlook.com>
2021-01-07 14:29:50 +0000

    burger menu

--- src/Components/NavBar/nav.js
+++ src/Components/NavBar/nav.js
@@ -21,61 +21,71 @@ export default function NavBar() {
     setUser(null);
   }
   return (
-    <nav className={style.nav}>
-      <div className="container">
-        <Link to="/"></Link>
-        {/* Search Bar */}
-        <div className={style.navContainer}>
-          <ul>
-            <li>
-              <img
-                className={style.img}
-                src={soc}
-                alt="School of Code Logo"
-                height="100"
-                width="100"
-              />
-            </li>
-            <Link to="/">
-              <li>
-                <span>Home</span>
-              </li>
-            </Link>
-            <Link to="/events">
-              <li>
-                <span>Events</span>
-              </li>
-            </Link>
-            <Link to="/profile">
-              <li>
-                <span>Profile</span>
-              </li>
-            </Link>
-            <Link to="/alumni">
-              <li>
-                <span>Alumni</span>
-              </li>
-            </Link>
-            <Link to="/contact">
-              <li>
-                <span>Contact</span>
-              </li>
-            </Link>
-            <li>
-              <img
-                className={style.cog}
-                src="https://www.flaticon.com/svg/static/icons/svg/3601/3601082.svg"
-              />
-            </li>
+    <div>
+      <input id="burger" type="checkbox" />
 
-            {user && (
-              <li className="links" onClick={signOut}>
-                <span>Log Out</span>
+      <label for="burger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </label>
+
+      <nav className={style.nav}>
+        <div className="container">
+          <Link to="/"></Link>
+          {/* Search Bar */}
+          <div className={style.navContainer}>
+            <ul>
+              <li>
+                <img
+                  className={style.img}
+                  src={soc}
+                  alt="School of Code Logo"
+                  height="100"
+                  width="100"
+                />
               </li>
-            )}
-          </ul>
+              <Link to="/">
+                <li>
+                  <span>Home</span>
+                </li>
+              </Link>
+              <Link to="/events">
+                <li>
+                  <span>Events</span>
+                </li>
+              </Link>
+              <Link to="/profile">
+                <li>
+                  <span>Profile</span>
+                </li>
+              </Link>
+              <Link to="/alumni">
+                <li>
+                  <span>Alumni</span>
+                </li>
+              </Link>
+              <Link to="/contact">
+                <li>
+                  <span>Contact</span>
+                </li>
+              </Link>
+              <li>
+                <img
+                  className={style.cog}
+                  src="https://www.flaticon.com/svg/static/icons/svg/3601/3601082.svg"
+                />
+              </li>
+
+              {user && (
+                <li className="links" onClick={signOut}>
+                  <span>Log Out</span>
+                </li>
+              )}
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
