0973076df82f76af5f5b80aa53d7ecc2aba70ddc
Luke Fantom <luke.fantom@gmail.com>
2021-01-07 16:34:27 +0000

    changed userimage stylesheet

--- src/Components/userImage/userImage.js
+++ src/Components/userImage/userImage.js
@@ -1,6 +1,7 @@
 //styling
-import style from "./userImage.module.css";
+// import style from "./userImage.module.css";
 import React, { useState, useEffect } from "react";
+import "./userImage.css";
 
 export default function UserImage({ user, width = "60px", onClick }) {
   const { profileimage, profileImage, name, cohort } = user;
@@ -8,15 +9,15 @@ export default function UserImage({ user, width = "60px", onClick }) {
 
   function styleCohort() {
     if (cohort === 1) {
-      setStyling(style.cohort1);
+      setStyling("cohort1");
     } else if (cohort === 2) {
-      setStyling(style.cohort2);
+      setStyling("cohort2");
     } else if (cohort === 3) {
-      setStyling(style.cohort3);
+      setStyling("cohort3");
     } else if (cohort === 4) {
-      setStyling(style.cohort4);
+      setStyling("cohort4");
     } else if (cohort === 5) {
-      setStyling(style.cohort5);
+      setStyling("cohort5");
     }
   }
 
@@ -24,18 +25,16 @@ export default function UserImage({ user, width = "60px", onClick }) {
     styleCohort();
   }, []);
 
-
   return (
     <div>
       <img
-        class={styling}
+        // class={styling}
         src={profileImage ? profileImage : profileimage}
         alt={`${name} profile`}
         width={width}
         onClick={onClick}
-//         className={style.profImg}
+        className={`profImg ${styling}`}
       />
     </div>
-
   );
 }
