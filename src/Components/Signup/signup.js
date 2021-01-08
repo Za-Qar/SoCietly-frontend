f1d3b71f8b0180127dd0f121247cdf9a763f8df0
= <za.qa@outlook.com>
2021-01-08 13:41:56 +0000

    signup test

--- src/Components/Signup/signup.js
+++ src/Components/Signup/signup.js
@@ -1,4 +1,5 @@
 //React
+import React from "react";
 import { useState } from "react";
 import { Redirect } from "react-router-dom";
 import { useForm } from "react-hook-form";
@@ -16,6 +17,13 @@ import Tags from "../../MaterialUi/tags/tags.js";
 
 // css
 import "./signup.css";
+
+// Mat ui
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
 
 export default function Signup({ signup, setSignup }) {
   // Context
@@ -114,6 +122,99 @@ export default function Signup({ signup, setSignup }) {
 
   return authUser ? (
     <div>
+      <React.Fragment>
+        <Typography variant="h6" gutterBottom>
+          Shipping address
+        </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="firstName"
+              name="firstName"
+              label="First name"
+              fullWidth
+              autoComplete="given-name"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="lastName"
+              name="lastName"
+              label="Last name"
+              fullWidth
+              autoComplete="family-name"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="address1"
+              name="address1"
+              label="Address line 1"
+              fullWidth
+              autoComplete="shipping address-line1"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              id="address2"
+              name="address2"
+              label="Address line 2"
+              fullWidth
+              autoComplete="shipping address-line2"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="city"
+              name="city"
+              label="City"
+              fullWidth
+              autoComplete="shipping address-level2"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="state"
+              name="state"
+              label="State/Province/Region"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="zip"
+              name="zip"
+              label="Zip / Postal code"
+              fullWidth
+              autoComplete="shipping postal-code"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="country"
+              name="country"
+              label="Country"
+              fullWidth
+              autoComplete="shipping country"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormControlLabel
+              control={
+                <Checkbox color="secondary" name="saveAddress" value="yes" />
+              }
+              label="Use this address for payment details"
+            />
+          </Grid>
+        </Grid>
+      </React.Fragment>
+
       <h1>Sign Up</h1>
       <button onClick={logout}>Return to Home</button>
       <div>
