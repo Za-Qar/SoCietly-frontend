6293dae3f66e3a25ceb9d1766c4663a783324a3b
Luke Fantom <luke.fantom@gmail.com>
2021-01-12 22:55:07 +0000

    fixed signup issue

--- src/Helpers/fetch.js
+++ src/Helpers/fetch.js
@@ -5,7 +5,7 @@ export default async function fetchData(path) {
   console.log("fetch");
   const res = await fetch(`${url}${path}`);
   const data = await res.json();
-
+  console.log(data);
   return new Promise(function (myResolve, myReject) {
     myResolve(data);
   });
