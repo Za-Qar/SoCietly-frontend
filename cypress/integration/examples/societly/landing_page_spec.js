d5699ab6f7afcfcaf7720057ce4546c83b4f2d16
mbrazze <bryan.beale88@gmail.com>
2021-01-12 09:30:34 +0000

    BB-FAL-162 front end testing

---
+++ cypress/integration/examples/societly/landing_page_spec.js
@@ -0,0 +1,30 @@
+// Testing that the URL brings us to the correct web page
+describe("The Landing Page", () => {
+  it("Successfully loads", () => {
+    cy.visit("/");
+  });
+});
+
+// Testing that clicking the button brings up the login page with login in the URL
+describe("Login Page", () => {
+  it('clicks the link "Continue with Google"', () => {
+    cy.visit("/");
+
+    cy.contains("Continue with Google").click();
+
+    //Should be on a new URL which includes '/login'
+    cy.url().should("include", "/login");
+  });
+});
+
+// // Testing events page
+
+// describe("Sign in successful", () => {
+//   it("Gets, type and asserts", () => {
+//     cy.visit("/");
+
+//     cy.contains("Continue with Google").click();
+
+//     cy.url().should("include", "/login");
+//   });
+// });
